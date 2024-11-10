3a827ce4ce1416c33137811e6e5b38a85b86aef5
Shish_Kaushik <76246051+ShisheerKaushik24@users.noreply.github.com>
2024-11-10 17:56:36 +0530

    Update train_model.py

--- Assignment/project/src/train_model.py
+++ Assignment/project/src/train_model.py
@@ -1,31 +1,8 @@
-import pandas as pd
-import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from feature_engineering import preprocess_features
-from data_preprocessing import load_data, split_data, save_data
 
-def train_model(data_path):
-    # Load and preprocess data
-    data = load_data(data_path)
-    data = preprocess_features(data)
-    train, test = split_data(data)
-    
-    X_train = train.drop('Churn', axis=1)
-    y_train = train['Churn']
-    X_test = test.drop('Churn', axis=1)
-    y_test = test['Churn']
-
-    # Train the model
+def train_model(X_train, y_train):
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
+    return model
 
-    # Evaluate and save model
-    accuracy = accuracy_score(y_test, model.predict(X_test))
-    print(f'Model Accuracy: {accuracy:.2f}')
-    
-    with open('models/churn_model.pkl', 'wb') as f:
-        pickle.dump(model, f)
-
-if __name__ == '__main__':
-    train_model('data/raw/churn_data.csv')
